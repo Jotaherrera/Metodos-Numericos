91403ea370770ee75021fd0f3dc415c8a675e7ef
Jotaherrera <juanherreramen13@gmail.com>
2023-02-15 07:18:26 -0500

    Fix algorithm rounding var

--- equationSolver.py
+++ equationSolver.py
@@ -3,8 +3,13 @@ import random as rnd
 xI = rnd.randint(-100, 100)
 
 
+# (3 * xI) - 27 = 9
+# (5 * xI) - 15 = 3
+# 3 * ((2 * xI) + 1) + 9 = -2
+
+
 def equation(xI):
-    return (3 * xI) - 27
+    return 3 * ((2 * xI) + 1) + 9
 
 
 # La ecuación siempre tiene que dar 0
@@ -16,16 +21,16 @@ else:
     while True:
         counter = counter + 1
 
-        res = abs(equation(xI))
+        res = equation(xI)
 
-        if res >= 0.0001:
-            x1 = xI + 0.2
+        if res > 0:
+            x1 = xI - 0.2
             xI = x1
         else:
-            x2 = xI - 0.2
+            x2 = xI + 0.2
             xI = x2
 
-        if abs(equation(xI)) <= 0.001:
+        if equation(round(xI)) == 0:
             print(
                 f"Felicidades, llegaste a la respuesta, es: {round(xI)} y el número de iteraciones fue: {counter}"
             )
