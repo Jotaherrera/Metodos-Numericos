c3fc28b5b5d1cb6de02a39cb4f2fb0b73e8fdf93
Jotaherrera <juanherreramen13@gmail.com>
2023-02-15 06:21:30 -0500

    Add equqtion solver

--- equationSolver.py
+++ equationSolver.py
@@ -1,6 +1,6 @@
 import random as rnd
 
-xI = rnd.randint(-10, 10)
+xI = rnd.randint(-100, 100)
 
 
 def equation(xI):
@@ -10,6 +10,7 @@ def equation(xI):
 # La ecuación siempre tiene que dar 0
 if abs(equation(xI)) <= 0.001:
     print(print(f"Felicidades, llegaste a la respuesta, es: {xI}"))
+
 else:
     counter = 0
     while True:
@@ -18,13 +19,14 @@ else:
         res = abs(equation(xI))
 
         if res >= 0.0001:
-            x1 = xI - 0.2
+            x1 = xI + 0.2
             xI = x1
         else:
-            x2 = xI + 0.2
+            x2 = xI - 0.2
             xI = x2
 
         if abs(equation(xI)) <= 0.001:
             print(
-                f"Felicidades, llegaste a la respuesta, es: {xI} y el número de iteraciones fue: {counter}"
+                f"Felicidades, llegaste a la respuesta, es: {round(xI)} y el número de iteraciones fue: {counter}"
             )
+            break
