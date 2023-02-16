3e51971fd33417ff5d9cc110a84a999e24449f07
jotaherrera <juanherreramen13@gmail.com>
2023-02-15 20:05:49 -0500

    Fix conditions

--- equationSolver.py
+++ equationSolver.py
@@ -9,10 +9,11 @@ xI = rnd.randint(-100, 100)
 
 
 def equation(xI):
-    return 3 * ((2 * xI) + 1) + 9
+    return (3 * xI) - 27
 
 
 # La ecuación siempre tiene que dar 0
+# Este algoritmo solo resuelve ecuaciones donde la variable corresponde a un número entero
 if abs(equation(xI)) <= 0.001:
     print(f"Felicidades, llegaste a la respuesta, es: {xI}")
 
@@ -24,10 +25,10 @@ else:
         res = equation(xI)
 
         if res > 0:
-            x1 = xI - 0.2
+            x1 = xI - 0.1
             xI = x1
         else:
-            x2 = xI + 0.2
+            x2 = xI + 0.1
             xI = x2
 
         if equation(round(xI)) == 0:
