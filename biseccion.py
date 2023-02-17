e787536858f1340f6048cb1a431639ad7c4a61ba
Jotaherrera <juanherreramen13@gmail.com>
2023-02-17 11:35:21 -0500

    Add binarySearch algorithm

---
+++ biseccion.py
@@ -0,0 +1,7 @@
+def biseccion():
+    xHigh = 0
+    xLow = 0
+    # Find numbers by random verifying if xHigh result is over 0 and xLow is under 0
+    # Then replace x
+    if xHigh > xLow:
+        xMiddle = (xHigh + xLow) / 2
