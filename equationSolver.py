96f209aab97a3bad5749b389d218fd2946ee5747
Jotaherrera <juanherreramen13@gmail.com>
2023-03-01 20:33:53 -0500

    Fix for float numbers

--- equationSolver.py
+++ equationSolver.py
@@ -1,6 +1,6 @@
 import random as rnd
 
-xI = rnd.randint(-10000, 10000)
+xI = rnd.randint(-1000, 1000)
 
 # Ecuaciones para testing:
 # (3 * xI) - 27 = 9
@@ -22,17 +22,15 @@ else:
     while True:
         counter = counter + 1
 
-        res = equation(xI)
-
-        if res > 0:
+        if equation(xI) > 0:
             x1 = xI - 0.1
             xI = x1
         else:
             x2 = xI + 0.1
             xI = x2
 
-        if equation(round(xI)) == 0:
+        if abs(equation(xI)) <= 0.001:
             print(
-                f"Felicidades, llegaste a la respuesta, es: {round(xI)} y el número de iteraciones fue: {counter}"
+                f"Felicidades, llegaste a la respuesta, es: {round(xI,2)} y el número de iteraciones fue: {counter}"
             )
             break
