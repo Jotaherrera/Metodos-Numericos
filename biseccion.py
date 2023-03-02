0f462262f8c7ea4520f4874d09f908a5e70b43a7
jotaherrera <juanherreramen13@gmail.com>
2023-03-01 19:45:36 -0500

    Refactor and optimization

--- biseccion.py
+++ biseccion.py
@@ -1,59 +1,51 @@
 import random as rnd
+import sys as sys
+
+sys.setrecursionlimit(10**6)
 
 # Ecuaciones de prueba, remplazar en el return de la función siguiente
 # (3 * xI) - 27 = 9
 # (5 * xI) - 15 = 3
 # 3 * ((2 * xI) + 1) + 9 = -2
+# (2 * xI) - 11 = 5.5
 
 
 # Función ecuación.
 def eq(xI):
-    return (3 * xI) - 27
+    return (2 * xI) - 11
 
 
 # Función de definición de pares de números aleatorios dentro de un rango especifico
 def rndNumbers():
-    xLow = rnd.randint(-100, 100)
-    xHigh = rnd.randint(xLow, 100)
+    xLow = rnd.randint(-1000, 1000)
+    while eq(xLow) > 0:
+        xLow = rnd.randint(-1000, 1000)
+
+    xHigh = rnd.randint(xLow, 1000)
+    while eq(xHigh) < 0:
+        xHigh = rnd.randint(xLow, 1000)
 
     print(f"* El rango de búsqueda es de {xLow} a {xHigh} ")
     return xLow, xHigh
 
 
-count = 0
-
-
 # Función recursiva de búsqueda bi-seccionada.
-def biseccion(xLow, xHigh):
-    global count
+def biseccion(xLow, xHigh, count=0):
     count = count + 1
 
-    if xHigh >= xLow:
-        xMiddle = (xHigh + xLow) // 2
+    xMiddle = (xHigh + xLow) / 2
 
-        res = eq(xMiddle)
-
-        if res == 0:
-            isInList = 1
-            return xMiddle, count, isInList
-        elif res > 0:
-            return biseccion(xLow, xMiddle - 1)
-        else:
-            return biseccion(xMiddle + 1, xHigh)
+    if abs(eq(xMiddle)) <= 0.001:
+        return xMiddle, count
+    elif eq(xMiddle) > 0.001:
+        return biseccion(xLow, xMiddle - 0.1, count)
     else:
-        # Cuando el número menor sobrepasa el mayor el resultado
-        # no se encuentra dentro de ese rango,por lo que se genera uno nuevo.
-        isInList = 0
-        print(f"X El elemento no estaba presente en el rango, generando uno nuevo... ")
-        return 0, count, 0
+        return biseccion(xMiddle + 0.1, xHigh, count)
 
 
-isInList = 0
+xLow, xHigh = rndNumbers()
+x, count = biseccion(xLow, xHigh)
 
-# Ciclo de ejecución de ambas funciones hasta que la función principal
-# devuelva el resultado correcto.
-while isInList == 0:
-    xLow, xHigh = rndNumbers()
-    x, count, isInList = biseccion(xLow, xHigh)
-
-print(f"» La X de la ecuación corresponde a {x} y el número de iteraciones fue {count}")
+print(
+    f"» La X de la ecuación corresponde a {round(x, 2)} y el número de iteraciones fue {count}"
+)
