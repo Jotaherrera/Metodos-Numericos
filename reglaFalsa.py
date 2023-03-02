763ed4077d1fa46a1704c531a30da3d9e41c9d72
jotaherrera <juanherreramen13@gmail.com>
2023-03-01 19:45:52 -0500

    Update Regla Falsa

--- reglaFalsa.py
+++ reglaFalsa.py
@@ -1,2 +1,48 @@
-# xA & xB.
-# xC = xA - (f(xA) * (xB - xA)/(f(xB) - f(xA))
+import random as rnd
+
+# Ecuaciones de prueba, remplazar en el return de la función siguiente
+# (3 * xI) - 27 = 9
+# (5 * xI) - 15 = 3
+# 3 * ((2 * xI) + 1) + 9 = -2
+# (2 * xI) - 11 = 5.5
+
+
+# Función ecuación.
+def eq(xI):
+    return (5 * xI) - 15
+
+
+# Función de definición de pares de números aleatorios dentro de un rango especifico
+def rndNumbers():
+    xLow = rnd.randint(-1000, 1000)
+    while eq(xLow) > 0:
+        xLow = rnd.randint(-1000, 1000)
+
+    xHigh = rnd.randint(xLow, 1000)
+    while eq(xHigh) < 0:
+        xHigh = rnd.randint(xLow, 1000)
+
+    print(f"* El rango de búsqueda es de {xLow} a {xHigh} ")
+    return xLow, xHigh
+
+
+# Función recursiva de búsqueda bi-seccionada.
+def biseccion(xLow, xHigh, count=0):
+    count = count + 1
+
+    xMiddle = xLow - (eq(xLow) * (xHigh - xLow) / (eq(xHigh) - eq(xLow)))
+
+    if abs(eq(xMiddle)) <= 0.001:
+        return xMiddle, count
+    elif eq(xMiddle) > 0.001:
+        return biseccion(xLow, xMiddle - 0.1, count)
+    else:
+        return biseccion(xMiddle + 0.1, xHigh, count)
+
+
+xLow, xHigh = rndNumbers()
+x, count = biseccion(xLow, xHigh)
+
+print(
+    f"» La X de la ecuación corresponde a {round(x, 2)} y el número de iteraciones fue {count}"
+)
