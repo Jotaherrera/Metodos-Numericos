0257abd05c458f839cddb4c92b40f71b80b7376d
jotaherrera <juanherreramen13@gmail.com>
2023-03-29 15:27:56 -0500

    Update vars and from of the output

--- Corte2/newtonRapshon.py
+++ Corte2/newtonRapshon.py
@@ -1,22 +1,24 @@
 import random as rnd
 
-x0 = rnd.randint(-100, 100)
+x0 = rnd.randint(-1000, 1000)
 
 
-def f(x):
-    return 2 * x**7 + 5 * x + 2
+def f(xI):
+    return xI**3 + 3 * xI**2 + 3 * xI + 1
 
 
-def fP(x):
-    return 14 * x**6 + 5
+def fP(xI):
+    return 3 * xI**2 + 6 * xI + 3
 
 
 def main(x0):
+    count = 0
     while True:
+        count += 1
         x1 = x0 - (f(x0) / fP(x0))
 
         if f(x0) == 0:
-            print(x1)
+            print(f"X = {round(x1, 2)}  Count = {count}")
             break
         else:
             x0 = x1
