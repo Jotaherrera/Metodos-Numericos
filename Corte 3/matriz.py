b3ad60c98b386cd01b29928e1ca78775c1d09cb3
Jotaherrera <juanherreramen13@gmail.com>
2023-04-28 11:28:51 -0500

    Add first exercise

---
+++ Corte 3/matriz.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# A = [[2, -1], [4, -1]]
+# print(A)
+# B = [[1], [7]]
+# print(B)
+# print("")
+
+# INV_A = np.linalg.inv(A)
+# print(INV_A)
+# print("")
+
+# Solution = np.dot(INV_A, B)
+# print(Solution)
+
+# A = [[3,2,1], [5,3,4], [1,1,-1]]
+# B = [[]]
