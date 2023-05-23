890645f79954a9bdd381f79f771c6ee398c60ee2
jotaherrera <juanherreramen13@gmail.com>
2023-05-23 14:16:14 -0500

    Update regression files

--- Corte3/linearRegresion.py
+++ Corte3/linearRegresion.py
@@ -1,78 +1,84 @@
-from IPython.display import clear_output
 import matplotlib.pyplot as plt
 import random as rnd
 import numpy as np
 
 
-X_real = np.linspace(0, 4, 100)
-Y_real = 2 * X_real + 1 + np.random.randn(*X_real.shape) * 0.2
+def main():
+    X_real = np.linspace(0, 4, 100)
+    Y_real = 2 * X_real + 1 + np.random.randn(*X_real.shape) * 0.2
 
-m = rnd.randint(0, 5)
-b = rnd.randint(0, 5)
+    m = rnd.randint(0, 5)
+    b = rnd.randint(0, 5)
 
-display_step = 50
-N = len(X_real)
-loss = []
+    display_step = 50
+    N = len(X_real)
+    loss = []
+
+    epochs = 500
+    learning_rate = 0.01
+    convergence_criteria = 1e-5
+
+    fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+
+    plt.ion()  # Turn on interactive mode
+
+    for step in range(epochs):
+        m_gradient = 0
+        b_gradient = 0
+        error = 0
+        difference = 0
+
+        for i in range(N):
+            Yp = m * X_real[i] + b
+            difference = Yp - Y_real[i]
+            error += difference**2
+            m_gradient += (2 / N) * difference * X_real[i]
+            b_gradient += (2 / N) * difference * 1
+
+        MSE = error / N
+
+        m -= m_gradient * learning_rate
+        b -= b_gradient * learning_rate
+
+        loss.append(np.abs(MSE))
+
+        if step % display_step == 0:
+            axes[0].set_xlim(0, 4)
+            axes[0].set_ylim(0, 10)
+            axes[0].cla()
+            axes[0].scatter(X_real, Y_real)
+            pred_x = [0, max(X_real)]
+            pred_y = m * X_real + b
+            axes[0].set_title("Epoch: {0}".format(step))
+            axes[0].plot(X_real, pred_y, "r")
+
+            plt.draw()
+            plt.pause(0.1)
+
+        if (
+            max(abs(learning_rate * m_gradient), abs(learning_rate * b_gradient))
+            < convergence_criteria
+        ):
+            plt.ioff()
+            plt.show()
+            break
+
+    plt.ioff()
+
+    print("Values: m =", m, "y b =", b)
+    print("Finished in", step, "iterations")
+    print("")
+    Y_prediction = m * X_real + b
+    MSE = np.mean(np.square(Y_prediction - Y_real))
+    print("MSE =", MSE)
+
+    axes[1].plot(loss)
+    axes[1].set_title("Cost (MSE)")
+    axes[1].set_xlabel("Epochs")
+    axes[1].set_ylabel("MSE")
+
+    plt.show()
 
 
-epochs = 500
-learning_rate = 0.01
-convergence_criteria = 1e-5
-
-fig, axes = plt.subplots(1, 2, figsize=(18, 5))
-
-
-for step in range(epochs):
-    m_gradient = 0
-    b_gradient = 0
-    error = 0
-    Diferencia = 0
-
-    for i in range(N):
-        Yp = m * X_real[i] + b
-        Diferencia = Yp - Y_real[i]
-        error += (Diferencia) ** 2
-        m_gradient += (2 / N) * (Diferencia) * X_real[i]
-        b_gradient += (2 / N) * (Diferencia) * 1
-
-    MSE = error / N
-
-    m = m - (m_gradient * learning_rate)
-    b = b - (b_gradient * learning_rate)
-
-    loss.append(np.abs(MSE))
-
-    if step % display_step == 0:
-        axes[0].set_xlim(0, 4)
-        axes[0].set_ylim(0, 10)
-        axes[0].cla()
-        axes[0].scatter(X_real, Y_real)
-        pred_x = [0, max(X_real)]
-        pred_y = m * X_real + b
-        axes[0].set_title("Epoca: {0}".format(step))
-        axes[0].plot(X_real, pred_y, "r")
-
-        fig.canvas.draw()
-        plt.pause(0.1)
-        clear_output()
-
-    if (
-        max(abs(learning_rate * m_gradient), abs(learning_rate * b_gradient))
-        < convergence_criteria
-    ):
-        plt.show()
-        break
-
-print("Values ", "m=", m, "y b=", b)
-print("Finished in ", step, " iteraciones ")
-print("")
-Y_prediction4 = m * X_real + b
-MSE = np.mean(np.square(Y_prediction4 - Y_real))
-print("MSE= ", MSE)
-
-axes[1].plot(loss)
-axes[1].set_title("Cost (MSE)")
-axes[1].set_xlabel("Epochs")
-axes[1].set_ylabel("MSE")
-
-plt.show()
+if __name__ == "__main__":
+    main()
--- Corte3/multiRegression.py
+++ Corte3/multiRegression.py
@@ -1,117 +1,102 @@
 import matplotlib.pyplot as plt
-import random as rnd
 import numpy as np
 
-# Delimiting X axes
-x1_min, x1_max = 0, 5
-x2_min, x2_max = 0, 3
 
-# ---------------------- Making the grid ---------------------------
-x1_dim, x2_dim = np.meshgrid(
-    np.arange(x1_min, x1_max, 0.1), np.arange(x2_min, x2_max, 0.1)
-)
-
-# ---------------------- Deconstructing  ---------------------------
-X1 = x1_dim.ravel()
-X2 = x2_dim.ravel()
-
-# ------------ Making and setting starting variables  --------------
-b = rnd.randint(0, 5)
-m1 = rnd.randint(0, 5)
-m2 = rnd.randint(0, 5)
-
-display_step = 50
-N = len(X1)
-loss = []
-
-epochs = 500
-learning_rate = 0.01
-convergence_criteria = 1e-5
-
-# --------------------------- Model ---------------------------------
-Y = 2 * X1 + 1.5 * X2 + 3 + np.random.randn(*X1.shape) * 0.3
-
-# --------------------- Setting graphs ------------------------------
-fig = plt.figure(figsize=(12, 6))
-
-# 3D mesh
-ax = fig.add_subplot(121, projection="3d")
-
-# Error Line
-ax2 = fig.add_subplot(122)
-
-# Real-time updating
-plt.ion()
+def getArtificialData():
+    x1_min, x1_max = 0, 5
+    x2_min, x2_max = 0, 3
+    x1_dim, x2_dim = np.meshgrid(
+        np.arange(x1_min, x1_max, 0.1), np.arange(x2_min, x2_max, 0.1)
+    )
+    X1 = x1_dim.ravel()
+    X2 = x2_dim.ravel()
+    Y = 2 * X1 + 1.5 * X2 + 3 + np.random.randn(*X1.shape) * 0.3
+    return X1, X2, Y
 
 
-# --------------------------- Learning -------------------------------
-for step in range(epochs):
-    m1_gradient = 0
-    m2_gradient = 0
-    b_gradient = 0
-    error = 0
-    dif = 0
+def graph(ax, ax2, X1, X2, Y, m1, m2, b, loss, step, MSE):
+    ax.clear()
+    ax.scatter(
+        X1,
+        X2,
+        m1 * X1 + m2 * X2 + b + np.random.randn(*X1.shape) * 0.3,
+        c="orange",
+        label="PC",
+    )
+    ax.scatter(X1, X2, 2 * X1 + 1.5 * X2 + 3, c="blue", label="Original")
+    ax.set_title("Artificial Data")
+    ax.set_xlabel("X1")
+    ax.set_ylabel("X2")
+    ax.set_zlabel("Y")
+    ax.set_xlim([0, 5])
+    ax.set_ylim([0, 3])
+    ax.set_zlim([2.5, 18])
+    ax.legend()
 
-    for i in range(N):
-        Yp = m1 * X1[i] + m2 * X2[i] + b
-        dif = Yp - Y[i]
-        error += (dif) ** 2
-        m1_gradient += (2 / N) * (dif) * X1[i]
-        m2_gradient += (2 / N) * (dif) * X2[i]
-        b_gradient += (2 / N) * (dif) * 1
+    ax2.clear()
+    ax2.plot(range(len(loss)), loss, label=f"MSE = {round(MSE, 2)}")
+    ax2.set_title("MSE vs Epochs")
+    ax2.set_xlabel("Epochs")
+    ax2.set_ylabel("MSE")
+    ax2.set_xlim([0, 500])
+    ax2.set_ylim([0, 5])
+    ax2.legend()
 
-    MSE = error / N
-
-    m1 = m1 - (m1_gradient * learning_rate)
-    m2 = m2 - (m2_gradient * learning_rate)
-    b = b - (b_gradient * learning_rate)
-
-    loss.append(np.abs(MSE))
-    if step % display_step == 0:
-        ax.clear()
-        ax.scatter(
-            X1,
-            X2,
-            m1 * X1 + m2 * X2 + b + np.random.randn(*X1.shape) * 0.3,
-            c="orange",
-            label="PC",
-        )
-        ax.scatter(X1, X2, 2 * X1 + 1.5 * X2 + 3, c="blue", label="Original")
-        ax.set_title("Artificial Data")
-        ax.set_xlabel("X1")
-        ax.set_ylabel("X2")
-        ax.set_zlabel("Y")
-        ax.set_xlim([0, 5])
-        ax.set_ylim([0, 3])
-        ax.set_zlim([2.5, 18])
-        ax.legend()
-
-        ax2.clear()
-        ax2.plot(range(len(loss)), loss, label=f"MSE = {round(MSE,2)}")
-        ax2.set_title("MSE vs Epochs")
-        ax2.set_xlabel("Epochs")
-        ax2.set_ylabel("MSE")
-        ax2.set_xlim([0, 500])
-        ax2.set_ylim([0, 5])
-        ax2.legend()
-
-        # -------------------- Adjusting graphs ----------------------------------
-        plt.tight_layout()
-        plt.draw()
-
-        # Refreshing every sec
-        plt.pause(1)
-
-    if (
-        max(abs(learning_rate * m1_gradient), abs(learning_rate * b_gradient))
-        < convergence_criteria
-    ):
-        break
-
-# Print model
-print(f"Y = {m1}*x1 + {m2}*x2 + {b}")
+    plt.tight_layout()
+    plt.draw()
+    plt.pause(1)
 
 
-# Deactivate graph updating
-plt.ioff()
-plt.show()
+def main():
+    X1, X2, Y = getArtificialData()
+
+    b, m1, m2 = np.random.randint(0, 5, size=3)
+    display_step = 50
+    N = len(X1)
+    loss = []
+    epochs = 500
+    learning_rate = 0.01
+    convergence_criteria = 1e-5
+
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.add_subplot(121, projection="3d")
+    ax2 = fig.add_subplot(122)
+    plt.ion()
+
+    for step in range(epochs):
+        m1_gradient = 0
+        m2_gradient = 0
+        b_gradient = 0
+        error = 0
+
+        for i in range(N):
+            Yp = m1 * X1[i] + m2 * X2[i] + b
+            dif = Yp - Y[i]
+            error += dif**2
+            m1_gradient += (2 / N) * dif * X1[i]
+            m2_gradient += (2 / N) * dif * X2[i]
+            b_gradient += (2 / N) * dif * 1
+
+        MSE = error / N
+        m1 -= m1_gradient * learning_rate
+        m2 -= m2_gradient * learning_rate
+        b -= b_gradient * learning_rate
+
+        loss.append(np.abs(MSE))
+
+        if step % display_step == 0:
+            graph(ax, ax2, X1, X2, Y, m1, m2, b, loss, step, MSE)
+
+        if (
+            max(abs(learning_rate * m1_gradient), abs(learning_rate * b_gradient))
+            < convergence_criteria
+        ):
+            break
+
+    print(f"Y = {m1}x1 + {m2}x2 + {b}")
+    plt.ioff()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
